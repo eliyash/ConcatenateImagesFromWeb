4e9a2429463f8a5fef0941da89f7fb95f6c6ec96
eliyash <eliyash3@gmail.com>
2015-06-01 14:43:40 -0700

    working image downloader + connector

--- concate.py
+++ concate.py
@@ -1,21 +1,38 @@
 from PIL import Image
+from downloadFile import download
 
-#opens an image:
-# im = Image.open("/home/eli/Workspace/Concatenator/images/test.jpg")
-im = Image.open('./images/test.jpg')
-#creates a new empty image, RGB mode, and size 400 by 400.
-new_im = Image.new('RGB', (400,400))
+heightOne = 1024
+weightOne = 1024
+heightFull = heightOne*3+536
+weightFull = weightOne*4+806
+# piecesH = heightFull/heightOne + 1
+# piecesW = weightFull/weightOne + 1
 
-#Here I resize my opened image, so it is no bigger than 100,100
-im.thumbnail((100,100))
-#Iterate through a 4 by 4 grid with 100 spacing, to place my image
-for i in xrange(0,500,100):
-    for j in xrange(0,500,100):
-        #I change brightness of the images, just to emphasise they are unique copies.
-        im=Image.eval(im,lambda x: x)
-        #paste the image at location i,j:
-        new_im.paste(im, (i,j))
+path = '/home/eli/Workspace/Concatenator/images/test.jpg'
+# im = Image.open('/home/eli/Workspace/Concatenator/images/test.jpg')
+
+new_im = Image.new('RGB', (weightFull,heightFull))
+
+
+startUrl = "http://192.114.7.88:2121/iipsrv?FIF=/operational_storage/derivative_copies/2014/06/26/file_3"
+conUrl = "/V1-FL12997"
+endUrl = ".ptif&JTL=6,"
+pageStart = 661
+index = 35
+for page in range(0,190):
+    for i in range(0,4):
+        for j in range(0,5):
+            print
+            try:
+                download(startUrl+str((pageStart+index-30)/100-4)+conUrl+str(pageStart+index)+endUrl+str(i*5+j),path)
+            except Exception as e:
+                print "error in" + startUrl+str((pageStart+index-30)/100-4)+conUrl+str(pageStart+index)+endUrl+str(i*5+j)
+                break
+            im=Image.open(path) #Image.eval(im,lambda x: x)
+            #paste the image at location i,j:
+            new_im.paste(im,(j*1024,i*1024))
+    new_im.save('/home/eli/Workspace/Concatenator/output/page'+str(index)+'.jpg')
+    index+=1
+
 
 # new_im.show()
-# new_im.save(open('/home/eli/Workspace/Concatenator/output/hg.jpg', 'rb'))
-new_im.save('./output/hg.jpg')
